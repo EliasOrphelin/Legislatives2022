3c2f7e9ec8637122d6ee4d2f85ebb8237c3de5ed
rozierguillaume <guillaume.rozier73@gmail.com>
2022-06-08 10:42:56 +0200

    Nettoyage

--- src/test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("https://raw.githubusercontent.com/EliasOrphelin/Legislatives2022/main/SondagesLegislatives2022.csv")
-df = df[df["type"]=="score"]
-df = df[df["nom_institut"]!="Cluster17"]
-df = df[df["parti"]=="Ensemble"]
-print(df)
